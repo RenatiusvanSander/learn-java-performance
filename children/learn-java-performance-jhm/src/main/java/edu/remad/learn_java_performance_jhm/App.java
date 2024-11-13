ecbea3f1a09a05338a531ddc8c56071c36b3e762
Remy Meier <remad@web.de>
2024-11-13 19:14:27 +0100

    master: initial project setup for parent and children

---
+++ children/learn-java-performance-jhm/src/main/java/edu/remad/learn_java_performance_jhm/App.java
@@ -0,0 +1,10 @@
+package edu.remad.learn_java_performance_jhm;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
